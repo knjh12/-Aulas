5639c4e05fb4f431b8ee9ed77793ee7b37a0cddf
betamoreira <79289691+betamoreira@users.noreply.github.com>
2021-10-21 17:34:22 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
-var backgroundImg, platform;
+var box1, box2, box3, box4, box5;
+var ground;
+var pig1, pig2;
+var log1, log2, log3, log4;
+var bird;
+var backgroundimg, platform;
+var restritoLog;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload(){
+    backgroundimg = loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -15,51 +21,63 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170);
+    ground = new Ground(600,height,1200,20);
+    restritoLog = new Log(230,180,80,PI/2);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+
+    pig1 = new Pig(810,350);
+    pig2 = new Pig(810,220);
 
     bird = new Bird(100,100);
 
-    platform = new Ground(150,305,300,170);
+    log1 = new Log(810,260,300,PI/2);
+    log2 = new Log(810,180,300,PI/2);
+    log3 = new Log(760,120,150,PI/7);
+    log4 = new Log(870,120,150,-PI/7);
 
+
+    var options = {
+        bodyA: bird.body,
+        bodyB: restritoLog.body,
+        stiffness: 0.04,
+        length: 10
+    }
+
+    var chain = Constraint.create(options);
+    World.add(world, chain);
 }
 
 function draw(){
-    background(backgroundImg);
+    background(backgroundimg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
+    platform.display();
+    ground.display(); 
+
     box1.display();
     box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
     box5.display();
-    log4.display();
-    log5.display();
+    
+    pig1.display();
+    pig2.display();
 
     bird.display();
-    platform.display();
+
+    log1.display();
+    log2.display();
+    log3.display();
+    log4.display();
+
+    restritoLog.display();
+
+    strokeWeight(3);
+    line(bird.body.position.x, bird.body.position.y, restritoLog.body.position.x, restritoLog.body.position.y);
 }
