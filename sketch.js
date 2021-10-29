91579e2b9496ff03c6dab571978ff5314aae906a
norberto <otsukikienai@gmail.com>
2021-10-29 10:41:22 -0300

    atualizacao_aula_29

--- sketch.js
+++ sketch.js
@@ -4,17 +4,12 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, box2, box3, box4, box5;
-var ground;
-var pig1, pig2;
-var log1, log2, log3, log4;
-var bird;
-var backgroundimg, platform;
-var restritoLog;
-var slingshot;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
 
-function preload(){
-    backgroundimg = loadImage("sprites/bg.png");
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -22,63 +17,61 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    platform = new Ground(150,305,300,170);
+
     ground = new Ground(600,height,1200,20);
-    //restritoLog = new Log(230,180,80,PI/2);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
     box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
 
-    pig1 = new Pig(810,350);
-    pig2 = new Pig(810,220);
+    bird = new Bird(200,50);
 
-    bird = new Bird(100,100);
-
-    log1 = new Log(810,260,300,PI/2);
-    log2 = new Log(810,180,300,PI/2);
-    log3 = new Log(760,120,150,PI/7);
-    log4 = new Log(870,120,150,-PI/7);
-
-    slingshot= new SlingShot(bird.body, {x:200,y:100});
-    
-
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    background(backgroundimg);
+    background(backgroundImg);
     Engine.update(engine);
-    
-    
-
-    platform.display();
-    ground.display(); 
-
+    strokeWeight(4);
     box1.display();
     box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
     box3.display();
     box4.display();
+    pig3.display();
+    log3.display();
+
     box5.display();
-    
-    pig1.display();
-    pig2.display();
+    log4.display();
+    log5.display();
 
     bird.display();
-
-    log1.display();
-    log2.display();
-    log3.display();
-    log4.display();
-
-    slingshot.display(); 
+    platform.display();
+    //log6.display();
+    slingshot.display();    
 }
+
 function mouseDragged(){
-     Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
-
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
-function mouseReleased(){
- slingshot.fly();
 
+
+function mouseReleased(){
+    slingshot.fly();
 }
